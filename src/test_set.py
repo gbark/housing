910d7d7489444092b856c16f617e83d90cce3184
Gustaf Barkeling <gustaf@frontengineering.com>
2019-12-21 18:30:16 -0500

    Tiny bit more done

--- src/test_set.py
+++ src/test_set.py
@@ -8,6 +8,6 @@ def test_set_check(identifier, test_ratio):
 
 
 def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data(id_column)
+    ids = data[id_column]
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[in_test_set]
+    return data.loc[~in_test_set], data.loc[in_test_set]
